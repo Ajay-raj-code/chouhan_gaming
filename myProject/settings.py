44782a921713c706cd050fd18b3d59bf66a80315
Ajay Raj <154655197+Ajay-raj-code@users.noreply.github.com>
2024-05-07 17:34:52 +0530

    Update settings.py
    set admin pannel

--- myProject/settings.py
+++ myProject/settings.py
@@ -122,8 +122,16 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 
-STATIC_ROOT = BASE_DIR / 'productionfiles'
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+if DEBUG:
+   STATICFILES_DIRS = [
+   BASE_DIR/ 'static',
+   ]
+else:
+   STATIC_ROOT = BASE_DIR/'static'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR/ 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
