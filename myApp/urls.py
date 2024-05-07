243197059f8334bf4a86a02c24f36ade9a4916f7
Ajay Raj <154655197+Ajay-raj-code@users.noreply.github.com>
2024-05-07 16:27:38 +0530

    Update urls.py

--- myApp/urls.py
+++ myApp/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('admin/', admin.site.urls),
+
     path("", views.home, name="home"),
     path("login/", views.loginPage, name="login"),
     path("registration/", views.registerPage, name="registration"),
